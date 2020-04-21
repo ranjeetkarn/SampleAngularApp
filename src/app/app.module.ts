bff60a04a280986df9818d7a9f311e2e3e42ce5c
Rohit Chopra <crohit92@gmail.com>
2020-04-21 12:31:24 +0530

    Content trasnclusion: Step - 1 Add rda-button without transclusion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ProductComponent } from './products/product.component';
-import { FormsModule} from '@angular/forms'
+import { FormsModule } from '@angular/forms';
 import { ProductFilterPipe } from './products/product-filter.pipe';
+import { RdaButtonModule } from './shared/components/rda-button/rda-button.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,ProductComponent, ProductFilterPipe
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule    
-  ],
+  declarations: [AppComponent, ProductComponent, ProductFilterPipe],
+  imports: [BrowserModule, FormsModule, RdaButtonModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/shared/components/rda-button/rda-button.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { RdaButtonComponent } from './rda-button.component';
+
+@NgModule({
+  declarations: [RdaButtonComponent],
+  exports: [RdaButtonComponent],
+})
+export class RdaButtonModule {}
